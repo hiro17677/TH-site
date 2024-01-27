a034aa4dea32c4fa7b7d2efb40980f52db71a660
unknown <hiro17677@gmail.com>
2024-01-28 02:58:55 +0800

    remove blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,55 +1,55 @@
-import React from "react"
-import { graphql, Link, useStaticQuery } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import React from "react"
+// import { graphql, Link, useStaticQuery } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
 
 
-const BlogPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allContentfulArticle(sort: { fields: publishDate, order: DESC }) {
-        edges {
-          node {
-            title
-            slug
-            publishDate(formatString: "MMMM Do, YYYY")
-            body {
-              childMarkdownRemark {
-                html
-              }
-            }
-          }
-        }
-      }
-      allContentfulCategory {
-        edges {
-          node {
-            title
-            slug
-          }
-        }
-      }
-    }
-  `)
+// const BlogPage = () => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       allContentfulArticle(sort: { fields: publishDate, order: DESC }) {
+//         edges {
+//           node {
+//             title
+//             slug
+//             publishDate(formatString: "MMMM Do, YYYY")
+//             body {
+//               childMarkdownRemark {
+//                 html
+//               }
+//             }
+//           }
+//         }
+//       }
+//       allContentfulCategory {
+//         edges {
+//           node {
+//             title
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  const articles = data.allContentfulArticle.edges
-  const categories = data.allContentfulCategory.edges
+//   const articles = data.allContentfulArticle.edges
+//   const categories = data.allContentfulCategory.edges
 
-  return (
-    <div className="wrapper mt-8 pb-24 pb-36">
-      {/* ... Your existing code ... */}
-      {articles.map(({ node }) => (
-        <div key={node.slug}>
-          {/* Use `Link` to navigate to the blog post using the `slug*/}
-          <Link to={`/blog/${node.slug}`}>
-            <h2>{node.title}</h2>
-            <p>{node.publishDate}</p>
-            <div dangerouslySetInnerHTML={{ __html: node.description.childMarkdownRemark.html }} />
-          </Link>
-        </div>
-      ))}
-      {/* ... Your existing code ... */}
-    </div>
-  )
-}
+//   return (
+//     <div className="wrapper mt-8 pb-24 pb-36">
+//       {/* ... Your existing code ... */}
+//       {articles.map(({ node }) => (
+//         <div key={node.slug}>
+//           {/* Use `Link` to navigate to the blog post using the `slug*/}
+//           <Link to={`/blog/${node.slug}`}>
+//             <h2>{node.title}</h2>
+//             <p>{node.publishDate}</p>
+//             <div dangerouslySetInnerHTML={{ __html: node.description.childMarkdownRemark.html }} />
+//           </Link>
+//         </div>
+//       ))}
+//       {/* ... Your existing code ... */}
+//     </div>
+//   )
+// }
 
-export default BlogPage
+// export default BlogPage
